cbe437cc9b3b13c0f25d78558ba378f0c11716f6
Xihai Luo <xihai.luo01@gmail.com>
2023-06-15 16:08:02 -0400

    version 1.0

---
+++ SolarSystem/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 CameraPosition;
+    [Header("Camera Settings")]
+    public float CameraSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            CameraPosition.z += (CameraSpeed / 10) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            CameraPosition.z -= (CameraSpeed / 10) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            CameraPosition.x -= (CameraSpeed / 10) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            CameraPosition.x += (CameraSpeed / 10) * Time.deltaTime;
+        }
+        transform.position = CameraPosition;
+    }
+}
